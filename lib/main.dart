a0f08f41d98cd9d875e9e0a0895c184e74fc6c76
FlutLab.io <>
2022-04-19 02:59:49 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -15,28 +15,40 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Alignment'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-
-  const MyHomePage({@required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: topLeft(
-        child: Text(
-          'Product',
+        appBar: AppBar(
+          // The title text which will be shown on the action bar
+          title: Text(title),
         ),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return Padding(
+                  padding: const EdgeInsets.only(
+                    top: 20,
+                  ),
+                  child: ListTile(
+                      leading: Image.network("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F393713192426994178%2F&psig=AOvVaw2T6OmEmdPa7yhtXmiCf21H&ust=1650267093033000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPimtJLKmvcCFQAAAAAdAAAAABAD"),
+                      title: const Text(
+                        "Black tiles",
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      subtitle: const Text(
+                        "Image for backround",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      )));
+            }));
   }
 }
