201ef665f120873e3524dc30151d5bec9412d573
FlutLab.io <>
2022-04-20 16:17:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -31,24 +31,27 @@ class MyHomePage extends StatelessWidget {
           // The title text which will be shown on the action bar
           title: Text(title),
         ),
+    return Center(
         body: ListView.builder(
             itemCount: 20,
             itemBuilder: (context, index) {
               return Padding(
                   padding: const EdgeInsets.only(
                   ),
-                  child: ListTile(
+                  child: ListView(
+                    children: <Widget>[
+                      ListTile(
                       leading: Image.network(
-                        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F393713192426994178%2F&psig=AOvVaw2T6OmEmdPa7yhtXmiCf21H&ust=1650267093033000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPimtJLKmvcCFQAAAAAdAAAAABAD",)                      
-                        title: const Text( "Japan",
+                        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F393713192426994178%2F&psig=AOvVaw2T6OmEmdPa7yhtXmiCf21H&ust=1650267093033000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPimtJLKmvcCFQAAAAAdAAAAABAD",)
+                        title: Text( "Japan,
                         overflow: TextOverflow.ellipsis,
                       ),
 
-                      subtitle: const Text( "Japan vibe",
+                      subtitle: Text( "Japan vibe",
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
                       ),
-                        ));
+                        )];
             }));
   }
 }
