4b23de0efc71e6593e58b80bf0ebe56ec795e7d5
FlutLab.io <>
2022-04-20 15:54:29 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({Key? key, this.title}) : super(key: key);
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -36,18 +36,19 @@ class MyHomePage extends StatelessWidget {
             itemBuilder: (context, index) {
               return Padding(
                   padding: const EdgeInsets.only(
-                    top: 20,
                   ),
                   child: ListTile(
                       leading: Image.network(
-                        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F393713192426994178%2F&psig=AOvVaw2T6OmEmdPa7yhtXmiCf21H&ust=1650267093033000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPimtJLKmvcCFQAAAAAdAAAAABAD",)                      title: const Text( "Japan",
-
+                        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F393713192426994178%2F&psig=AOvVaw2T6OmEmdPa7yhtXmiCf21H&ust=1650267093033000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPimtJLKmvcCFQAAAAAdAAAAABAD",)                      
+                        title: const Text( "Japan",
                         overflow: TextOverflow.ellipsis,
                       ),
+
                       subtitle: const Text( "Japan vibe",
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
-                        )));
+                      ),
+                        ));
             }));
   }
 }
